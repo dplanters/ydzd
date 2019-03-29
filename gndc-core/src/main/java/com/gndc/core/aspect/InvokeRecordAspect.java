ebf6a9a4186eafd0546751065b7929f95d410109
jingkaihui <jingkaihui@adpanshi.com>
2019-03-29 14:38:43 +0800

    参数打印优化

--- gndc-core/src/main/java/com/gndc/core/aspect/InvokeRecordAspect.java
+++ gndc-core/src/main/java/com/gndc/core/aspect/InvokeRecordAspect.java
@@ -4,64 +4,72 @@ import cn.hutool.core.util.StrUtil;
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.serializer.JSONSerializer;
 import com.alibaba.fastjson.serializer.SimplePropertyPreFilter;
+import com.gndc.common.enums.ResultCode;
+import com.gndc.common.exception.HjException;
 import lombok.extern.slf4j.Slf4j;
-import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 
 /**
- *
+ * 用于打印方法执行前参数及执行后的结果
  */
 @Aspect
 @Component
 @Slf4j
 public class InvokeRecordAspect {
 
-    // 申明一个切点 里面是 execution表达式
+    /**
+     *
+     * 申明一个切点 里面是 execution表达式
+     */
     @Pointcut("execution(public * com.gndc.core.controller.*.*.*.*(..))")
     private void controllerAspect() {
     }
 
-    // 请求method前打印内容
-    @Before(value = "controllerAspect()")
-    public void methodBefore(JoinPoint joinPoint) {
+    @ResponseBody
+    @Around("controllerAspect()")
+    public Object isAccessMethod(ProceedingJoinPoint joinPoint) throws Throwable {
         ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder
                 .getRequestAttributes();
         HttpServletRequest request = requestAttributes.getRequest();
-        log.info("===============请求内容===============");
+        Object result;
         try {
+            log.info("<------请求内容------");
             // 打印请求内容
             log.info("请求地址:" + request.getRequestURL().toString());
             log.info("请求方式:" + request.getMethod());
             log.info("请求类方法:" + joinPoint.getSignature());
-            log.info("请求类方法参数:" + JSONObject.toJSONString(joinPoint.getArgs(), new SimplePropertyPreFilter() {
+            Object[] args = joinPoint.getArgs();
+            log.info("请求类方法参数:" + JSONObject.toJSONString(args, new SimplePropertyPreFilter() {
                 @Override
                 public boolean apply(JSONSerializer serializer, Object object, String name) {
                     return !StrUtil.containsAnyIgnoreCase(name, "rights");
                 }
             }));
+            log.info("<------请求内容------");
+
+            result = joinPoint.proceed(args);
+
+            log.info("------返回内容------>");
+            try {
+                log.info("Response内容:" + JSONObject.toJSONString(result));
+            } catch (Exception e) {
+                log.error("###LogAspectServiceApi.class methodAfterReturning() ### ERROR:", e);
+            }
+            log.info("------返回内容------>");
         } catch (Exception e) {
-            log.error("###LogAspectServiceApi.class methodBefore() ### ERROR:", e);
+            log.error("参数打印失败", e);
+            throw new HjException(ResultCode.SYSTEM_BUSY);
         }
-        log.info("===============请求内容===============");
+        return result;
     }
 
-    // 在方法执行完结后打印返回内容
-    @AfterReturning(returning = "o", pointcut = "controllerAspect()")
-    public void methodAfterReturning(Object o) {
-        log.info("--------------返回内容----------------");
-        try {
-            log.info("Response内容:" + JSONObject.toJSONString(o));
-        } catch (Exception e) {
-            log.error("###LogAspectServiceApi.class methodAfterReturning() ### ERROR:", e);
-        }
-        log.info("--------------返回内容----------------");
-    }
 }
