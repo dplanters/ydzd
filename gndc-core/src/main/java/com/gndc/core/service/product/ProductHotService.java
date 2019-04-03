bd68ff80980c3a74149abb9a65c0fe0d099a956a
jingkaihui <jingkaihui@adpanshi.com>
2019-04-03 11:03:16 +0800

    热推产品添加时过滤已热推的产品

---
+++ gndc-core/src/main/java/com/gndc/core/service/product/ProductHotService.java
@@ -0,0 +1,8 @@
+package com.gndc.core.service.product;
+
+import com.gndc.common.service.BaseService;
+import com.gndc.core.model.ProductHot;
+
+public interface ProductHotService extends BaseService<ProductHot, Integer> {
+
+}
