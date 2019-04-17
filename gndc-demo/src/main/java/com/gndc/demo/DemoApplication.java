401f87d6171a61d1750290b6e890b3b076d181b9
王洁 <wangjie2355@adpanshi.com>
2019-04-17 11:30:29 +0800

    解决报错

--- gndc-demo/src/main/java/com/gndc/demo/DemoApplication.java
+++ gndc-demo/src/main/java/com/gndc/demo/DemoApplication.java
@@ -1,5 +1,7 @@
 package com.gndc.demo;
 
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import tk.mybatis.spring.annotation.MapperScan;
